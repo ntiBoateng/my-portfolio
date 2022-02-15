b74c2f220ee3b74e06f748b86c9dfad28896ff40
ntiBoateng <gideon.boateng@amalitech.org>
2022-02-15 06:25:47 -0800

    removed settings.py

--- sey.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-time = datetime.datetime.now().time()
-morning = time.replace(hour=7, minute=45, second=0, microsecond=0)
-if time >= morning:
-    print("turn on {} >= {}".format(time,morning))
-else:
-    print("turn off {} <= {}".format(time,morning))
